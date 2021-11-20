8e163e5e4958574a8e18846fddb7a8dfb0347912
Harsh Dave <harshdave71720@gmail.com>
2021-11-20 18:29:30 +0530

    Added category route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { ExpensesPaginatorComponent } from './expenses-paginator/expenses-paginator.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 const routes : Routes = [
   { path : 'expenses', component : ExpensesComponent},
   // { path : 'expenses/:id', component : ExpenseComponent},
   { path : '', redirectTo : '/expenses', pathMatch : 'full'},
-  { path : 'expenses/paged', component : ExpensesPaginatorComponent}
+  { path : 'expenses/paged', component : ExpensesPaginatorComponent},
+  { path : 'categories', component : CategoriesComponent}
 ];
 
 @NgModule({
