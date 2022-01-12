b93d1aa8d88dda3d754d15bb090190ec0edb6f21
Harsh Dave <harshdave71720@gmail.com>
2022-01-12 23:43:24 +0530

    Wired FormComponent to paginator for refresh and add

--- src/app/expenses-paginator/expenses-paginator.component.ts
+++ src/app/expenses-paginator/expenses-paginator.component.ts
@@ -3,12 +3,8 @@ import { Expense, IExpense } from 'src/entities/expense';
 import { ExpenseService } from '../expense.service';
 import { Router } from '@angular/router';
 import { ICategory } from 'src/entities/category';
-import { ThrowStmt } from '@angular/compiler';
 import { CategoryService } from '../category.service';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ExpenseForm } from 'src/forms/expenseForm';
 
 @Component({
   selector: 'app-expenses-paginator',
@@ -18,14 +14,12 @@ import { ExpenseForm } from 'src/forms/expenseForm';
 export class ExpensesPaginatorComponent implements OnInit {
   categories : ICategory[] = [];
   expenses : IExpense[] = [];
-  expenseForm : ExpenseForm;
   newExpenseSelected : boolean = false;
   totalCount : number = 0;
   pageSize : number = 5;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private readonly expenseService : ExpenseService, private categoryService : CategoryService) {
-    this.expenseForm = new ExpenseForm(new Expense());
   }
 
   async ngOnInit(): Promise<void> {
@@ -49,24 +43,9 @@ export class ExpensesPaginatorComponent implements OnInit {
     e.subscribe(expenses => this.expenses = expenses);
   }
 
-  newClicked() : void
+  async onAdd(exp : IExpense) : Promise<void>
   {
-    this.newExpenseSelected = !this.newExpenseSelected;
-  }
-
-  async addExpenseClicked() : Promise<void>
-  {
-    if(this.expenseForm.IsInvalid)
-    {
-      this.expenseForm.form.markAllAsTouched();
-      this.expenseForm.form.updateValueAndValidity();
-      return;
-    }
-    // this.expenseForm.form.reset();
-
-    await this.expenseService.addExpense(this.expenseForm.getExpense());
+    await this.expenseService.addExpense(exp);
     await this.refreshExpenses();
-    this.newClicked();
-    this.expenseForm.refresh();
   }
 }
