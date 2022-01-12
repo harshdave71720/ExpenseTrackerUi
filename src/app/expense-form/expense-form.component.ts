f1b41c1289aba3589f3d6704b9d7dfcd6345c1a9
Harsh Dave <harshdave71720@gmail.com>
2022-01-12 17:16:05 +0530

    Added reactive form bindings

--- src/app/expense-form/expense-form.component.ts
+++ src/app/expense-form/expense-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Category, ICategory } from 'src/entities/category';
 import { Expense } from 'src/entities/expense';
 
 @Component({
@@ -8,8 +10,24 @@ import { Expense } from 'src/entities/expense';
 })
 export class ExpenseFormComponent implements OnInit {
   @Input() expense : Expense = new Expense();
-  constructor() { }
+  @Input() categories : ICategory[];
+  form : FormGroup;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.form = new FormGroup(
+      {
+        amount : new FormControl(this.expense.amount),
+        date : new FormControl(this.expense.date),
+        categoryName : new FormControl(this.expense.categoryName),
+        description : new FormControl(this.expense.description)
+      }
+    );
+  }
+
+  onSubmit() : void {
+    let e : Expense = this.form.value;
   }
 }
