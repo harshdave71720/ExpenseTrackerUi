eaec1335c49ff081c24fb23fefb3acd6f459840d
Harsh Dave <harshdave71720@gmail.com>
2022-02-26 14:16:02 +0530

    Added User name and login logout on nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SecurityService } from './services/security.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExpenseTrackerUi';
+
+  constructor(public securityService : SecurityService) {}
+
+  logout() : void {
+    this.securityService.logout();
+  }
 }
