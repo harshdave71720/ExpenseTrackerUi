62cb135d558e58a6b105690a5cb9eec6a57106f7
Harsh Dave <harshdave71720@gmail.com>
2022-03-02 18:57:32 +0530

    Added logic to save token

--- src/app/services/security.service.ts
+++ src/app/services/security.service.ts
@@ -1,29 +1,85 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
-import { User } from "src/entities/user";
+import { ApplicationUser } from "src/entities/applicationUser";
+import { UserRegister } from "src/entities/userRegister";
+import jwt_decode from 'jwt-decode';
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class SecurityService {
-  user : User;
+  jwt_key : string = "JWT_TOKEN";
+  user : ApplicationUser;
+  baseUrl : string = "https://localhost:5001";
 
-  login() : Observable<User>
-  {
-    if(this.user != null)
-      return of(this.user);
+  constructor(private readonly httpClient : HttpClient, private readonly router : Router){}
 
-    this.user = new User();
-    this.user.email = "harshdave71720@gmail.com";
-    this.user.firstname = "Harsh";
-    this.user.lastname = "Dave";
-    this.user.isAuthenticated = true;
-    return of(this.user);
+  login(email : string, password : string, returnUrl : string) {
+    let token = localStorage.getItem(this.jwt_key);
+    if(token)
+    {
+      let user = this.decodeToken(token);
+      if(user)
+        throw Error(`User ${user.firstname} Is Already Logged In, Please Log Out First`);
+    }
+
+    this.GetTokenAndUserInfo({ email : email, password : password })
+      .subscribe(user => {
+        this.user = user;
+        localStorage.setItem(this.jwt_key, user.bearerToken);
+        this.redirect(returnUrl);
+      });
   }
 
   logout() {
-    this.user = null;
+    localStorage.removeItem(this.jwt_key);
   }
 
-  register(user : User) {
-    this.user = user;
+  register(user : UserRegister, returnUrl : string) {
+    if(this.user)
+      throw Error(`User ${this.user.firstname} is already logged in. Please logout first`);
+
+    this.httpClient.post(`${this.baseUrl}/user/register`, user)
+    .subscribe(() => this.login(user.email, user.password, returnUrl));
   }
+
+  private GetTokenAndUserInfo(credentials : object) : Observable<ApplicationUser | undefined> {
+    return this.httpClient.post<TokenResponse>(`${this.baseUrl}/user/token`, credentials)
+      .pipe(
+        map(t => {
+          if(!t.token)
+            return undefined;
+          return this.decodeToken(t.token);
+        })
+      );
+  }
+
+  private decodeToken(token : string) : ApplicationUser {
+    let jwt: { [key: string]: string } = jwt_decode(token);
+    let user = new ApplicationUser();
+    user.firstname = jwt.FirstName;
+    user.lastname = jwt.LastName;
+    user.bearerToken = token;
+    return user;
+  }
+
+  public getLoggedInUser() : ApplicationUser {
+    let token = localStorage.getItem(this.jwt_key);
+    if(token)
+      return this.decodeToken(token);
+    return undefined;
+  }
+
+  private redirect(returnUrl : string) : void {
+     if(returnUrl)
+        this.router.navigate([returnUrl]);
+      else
+        this.router.navigate(['']);
+  }
+
+}
+
+class TokenResponse {
+  token : string;
 }
