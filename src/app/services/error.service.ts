1ccc59a966f895111334490931445933fa32ea48
Harsh Dave <harshdave71720@gmail.com>
2022-03-17 08:43:06 +0530

    redirect to login after token expires

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,15 +1,20 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { EMPTY } from "rxjs";
 import { IResponse } from "src/entities/Response";
 
 @Injectable()
 export class ErrorService {
-  constructor(private readonly toastr : ToastrService){}
+  constructor(private readonly toastr : ToastrService, private readonly router : Router){}
 
   handleError(errorResponse : HttpErrorResponse, toastr : ToastrService = null)
   {
+    if(errorResponse.status == 401)
+    {
+      this.router.navigate(['login']);
+    }
     let response = errorResponse.error as IResponse<any>;
     if(errorResponse.status != 0 && response?.errors)
     {
